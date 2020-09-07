2f796224a17e72c754eced9afc4a8d1c9486dc5a
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-09-07 20:38:34 -0300

    Fazendo a movimentação da cobra

--- snake/script.js
+++ snake/script.js
@@ -1,13 +1,49 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); // cria uma plataforma
 let box = 32;
+snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+} 
+
+let direction ="right";
 
 function createBG() {
     // coloca uma cor
     context.fillStyle = "lightblue"; 
-    
-    //fillRect tem 4 parametros altura, largura,x e y
+
+    //fillRect tem 4 parametros x, y, largura e altura
     context.fillRect(0, 0, 16 * box, 16 * box); 
 }
 
-createBG();
+function createSnake() {
+    for(let i = 0; i < snake.length; i++) {
+        context.fillStyle = "yellow";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }  
+}
+
+function startGame() {
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction === "right") snakeX += box;
+    if(direction === "left") snakeX -= box;
+    if(direction === "up") snakeY -= box;
+    if(direction === "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
