11a516f88e2e6a73be0e83c1c13ab07731252b26
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-09-07 22:20:13 -0300

    Configuração dos movimentos e do game over

--- snake/script.js
+++ snake/script.js
@@ -49,6 +49,14 @@ function startGame() {
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction =="down") snake[0].y = 0;
     if(snake[0].y < 0 && direction =="up") snake[0].y = 16 * box;
+
+        for( let i = 1; i < snake.length; i++) {
+            if(snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+                clearInterval(game);
+                alert("Game over :(");
+            }
+        }
+
     createBG();
     createSnake();
     drawFood();
@@ -61,8 +69,13 @@ function startGame() {
     if(direction === "up") snakeY -= box;
     if(direction === "down") snakeY += box;
 
-    snake.pop();
-
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop(); 
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+    
     let newHead = {
         x: snakeX,
         y: snakeY
